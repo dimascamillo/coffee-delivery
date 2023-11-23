b238d7d448d01e531e5d5262d57e9a357bae1ab8
Dimas Camillo <dimas.camillo@sonda.com>
2023-11-23 17:23:02 -0300

    Configuration project

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,17 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { BrowserRouter } from "react-router-dom";
-// import { Router } from "./Router";
+import { Router } from "./Router";
 // import { CyclesContextProvider } from "./contexts/CyclesContext";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      {/* <BrowserRouter>
-        <CyclesContextProvider>
-          <Router />
-        </CyclesContextProvider>
-      </BrowserRouter> */}
-      <h1>Teste 2</h1>
+      <BrowserRouter>
+        {/* <CyclesContextProvider> */}
+        <Router />
+        {/* </CyclesContextProvider> */}
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
