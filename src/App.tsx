848163928c24776a5fbb1b9bad18d960c3c945be
Dimas Camillo <dimas.camillo@sonda.com>
2023-11-23 17:17:22 -0300

    New features

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
-export function App() {
-  return <h1>Teste</h1>
-}
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { BrowserRouter } from "react-router-dom";
+// import { Router } from "./Router";
+// import { CyclesContextProvider } from "./contexts/CyclesContext";
 
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      {/* <BrowserRouter>
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
+      </BrowserRouter> */}
+      <h1>Teste 2</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
